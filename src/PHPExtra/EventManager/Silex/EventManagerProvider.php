88d750a15583fcfa70220c967a84c8e0b81917cf
Jacek Kobus <kobus.jacek@gmail.com>
2014-04-21 00:31:14 +0200

    renamed service class

--- src/PHPExtra/EventManager/Silex/EventManagerProvider.php
+++ src/PHPExtra/EventManager/Silex/EventManagerProvider.php
@@ -2,62 +2,14 @@
 
 namespace PHPExtra\EventManager\Silex;
 
-use PHPExtra\EventManager\EventManager;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
 
 /**
- * The SilexProvider class
+ * The EventManagerProvider class
  *
+ * @deprecated use EventManagerServiceProvider
  * @author Jacek Kobus <kobus.jacek@gmail.com>
  */
-class EventManagerProvider implements ServiceProviderInterface
+class EventManagerProvider extends EventManagerServiceProvider
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function register(Application $app)
-    {
-        $app['dispatcher_class'] = 'PHPExtra\\EventManager\\Silex\\CustomEventDispatcher';
-
-        $app['event_manager'] = $app->share(
-            function (Application $app) {
-                $em = new EventManager();
-
-                if ($app['debug'] == true) {
-                    $em->setThrowExceptions(true);
-                }
-
-                if ($app['logger'] !== null) {
-                    $em->setLogger($app['logger']);
-                }
-
-                return $em;
-            }
-        );
-
-        $app['event_manager.proxy_mapper'] = $app->share(
-            function (Application $app) {
-                return new ProxyMapper();
-            }
-        );
-
-        $app->extend(
-            'dispatcher',
-            function (CustomEventDispatcher $dispatcher, Application $app) {
-                $dispatcher
-                    ->setProxyMapper($app['event_manager.proxy_mapper'])
-                    ->setEventManager($app['event_manager']);
-
-                return $dispatcher;
-            }
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function boot(Application $app)
-    {
-    }
 }
---
+++ src/PHPExtra/EventManager/Silex/EventManagerServiceProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace PHPExtra\EventManager\Silex;
+
+use PHPExtra\EventManager\EventManager;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * The SilexProvider class
+ *
+ * @author Jacek Kobus <kobus.jacek@gmail.com>
+ */
+class EventManagerServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register(Application $app)
+    {
+        $app['dispatcher_class'] = 'PHPExtra\\EventManager\\Silex\\CustomEventDispatcher';
+
+        $app['event_manager'] = $app->share(
+            function (Application $app) {
+                $em = new EventManager();
+
+                if ($app['debug'] == true) {
+                    $em->setThrowExceptions(true);
+                }
+
+                if ($app['logger'] !== null) {
+                    $em->setLogger($app['logger']);
+                }
+
+                return $em;
+            }
+        );
+
+        $app['event_manager.proxy_mapper'] = $app->share(
+            function (Application $app) {
+                return new ProxyMapper();
+            }
+        );
+
+        $app->extend(
+            'dispatcher',
+            function (CustomEventDispatcher $dispatcher, Application $app) {
+                $dispatcher
+                    ->setProxyMapper($app['event_manager.proxy_mapper'])
+                    ->setEventManager($app['event_manager']);
+
+                return $dispatcher;
+            }
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(Application $app)
+    {
+    }
+}
